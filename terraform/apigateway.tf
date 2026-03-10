7dfe6faeb33bd834219d19c0725962ac68d6b3cf
kayanumaYoshimoto <hyeongwonchoe901@gmail.com>
2026-03-11 00:17:57 +0900

    feat: api gateway設定値

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -4,3 +4,45 @@ resource "aws_apigatewayv2_api" "api" {
 
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_integration" "lambda" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  integration_type = "AWS_PROXY"
+
+  integration_uri = aws_lambda_function.hello.invoke_arn
+
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "hello" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  route_key = "GET /hello"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  name = "$default"
+
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api" {
+
+  statement_id = "AllowAPIGatewayInvoke"
+
+  action = "lambda:InvokeFunction"
+
+  function_name = aws_lambda_function.hello.function_name
+
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
