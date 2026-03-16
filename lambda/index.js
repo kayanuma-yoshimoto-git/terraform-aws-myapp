035bfb4b96f8f01a128337cffe8722b56bc0d241
Yoshimoto Kayanuma <hyeongwonchoe901@gmail.com>
2026-03-16 21:12:20 +0900

    feat:update lambda

--- lambda/index.js
+++ lambda/index.js
@@ -2,10 +2,12 @@ const AWS = require("aws-sdk");
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+const TABLE_NAME = process.env.TABLE_NAME;
+
 exports.handler = async () => {
 
   const params = {
-    TableName: "encrypted-data",
+    TableName: TABLE_NAME,
     Item: {
       id: "1",
       encrypted_value: "test-data"
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,17 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda"
+  output_path = "../lambda/index.zip"
+}
 resource "aws_lambda_function" "payment" {
 
   function_name = "payment-${var.environment}"
 
-  filename = "../lambda/index.zip"
-  handler  = "index.handler"
-  runtime  = "nodejs18.x"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  handler = "index.handler"
+  runtime = "nodejs18.x"
 
   role = aws_iam_role.lambda_role.arn
 
